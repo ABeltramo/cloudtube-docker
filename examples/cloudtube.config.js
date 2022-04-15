b59cbee5776e75873a45cf2ce99a9ecb9a6cd992
Kayoo <jeri.dsql@gmail.com>
2022-04-15 10:29:44 +0200

    Update cloudtube.config.js
    If docker is not in same network you need to also set `local_instance_origin` to avoid this error : 
    ```
    FetchError: request to http://localhost:3000/api/v1/channels/UChV2oq_a-UZfJF-UiW0u-DQ/latest failed, reason: connect ECONNREFUSED 127.0.0.1:3000
    ```

--- examples/cloudtube.config.js
+++ examples/cloudtube.config.js
@@ -1,13 +1,16 @@
 module.exports = {
-	/*
-	  Copy this file to `config.js`, and add options here.
-	  They'll override the options from `utils/constants.js`.
-	  For example, the next block changes the default instance.
-	*/
+        /*
+          Copy this file to `config.js`, and add options here.
+          They'll override the options from `utils/constants.js`.
+          For example, the next block changes the default instance.
+        */
 
-	user_settings: {
-		instance: {
-			default: "http://newleaf:3000"
-		}
-	}
+        user_settings: {
+                instance: {
+                        default: "http://newleaf:3000"
+                }
+        },
+        server_setup: {
+                local_instance_origin: "http://newleaf:3000"
+        }
 }
